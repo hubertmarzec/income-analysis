b2d25d02666389ce949e4ffccc00ce9aa36c97af
Tomasz Kowalczy <tomkowalczyk@hotmail.com>
2018-09-07 16:02:55 +0200

    file structure

---
+++ src/app.py
@@ -0,0 +1,8 @@
+#
+#read csv
+#group by account No
+#agr by sum
+#print output
+def app(csv_path):
+
+	return [1,2,3]
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from app import app
+result=app('../data/Historia_Rachunku_180206_211715')
+print(result)
+print('test')
