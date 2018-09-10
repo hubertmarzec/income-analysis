12bbf4853a020b820510f406971b0ef1999ac428
Tomasz Kowalczy <tomkowalczyk@hotmail.com>
2018-09-10 11:23:25 +0200

    new changes

--- src/app.py
+++ src/app.py
@@ -4,5 +4,34 @@
 #agr by sum
 #print output
 def path_to_data(csv_path):
-	data= read_csv(csv_path, sep=',', decimal='.', index_col=0)
+	import csv
+	with open (csv_path) as csvfile
+	data= csv.reader(csvfile, sep=',', decimal='.', index_col=0)
+
 	return data
+
+
+
+def pick_wanted_cols(data, index_for_account, index_for_name, index_for_amount):
+
+	data[index_for_account]='account_no'
+	data[index_for_name]='name'
+	data[index_for_amount]='amount'
+
+	data=data[:,['account_no','name','amount']].index_col=0
+	data=data.values
+
+	return data
+
+def group_by_account_no(data):
+	
+	data.group_by(data['account_no'])
+
+	return grouped_data
+
+
+def sum_amount(data):
+
+	data = data.sum(data['amount'])
+
+	return summed_amount_data
